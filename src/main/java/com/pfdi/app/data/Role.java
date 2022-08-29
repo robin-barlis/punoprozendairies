be4d761636e70a85bbb7bf36cb78066d167e0858
start.vaadin.com <no-reply@vaadin.com>
2022-08-29 12:24:24 +0000

    Generated project

---
+++ src/main/java/com/pfdi/app/data/Role.java
@@ -0,0 +1,5 @@
+package com.pfdi.app.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/pfdi/app/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pfdi.app.data.service;
+
+import com.pfdi.app.data.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}
